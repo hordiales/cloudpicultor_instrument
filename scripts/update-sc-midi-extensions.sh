87e1aedbfc86cebd5b8a84356ddd998051cd495b
Hernán Ordiales <hordiales@gmail.com>
2019-02-16 17:45:53 -0300

    script fix

--- scripts/update-sc-midi-extensions.sh
+++ scripts/update-sc-midi-extensions.sh
@@ -7,5 +7,6 @@ EXTENSION_PATH=~/.local/share/SuperCollider/Extensions/
 #Bela
 #EXTENSION_PATH=/usr/local/share/SuperCollider/Extensions
 
+mkdir $EXTENSION_PATH
 echo "Copying to $EXTENSION_PATH"
 cp -v ui/*.sc $EXTENSION_PATH
